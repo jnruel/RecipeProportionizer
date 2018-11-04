4a1b54196694c215f6a55968dad138860c7d2c4c
jruel <jruel@sandstormdesign.com>
2018-11-04 16:07:56 -0600

    webpack separate environment configs + webpack dev server set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,3 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
-};
+module.exports = (env) => {
+  return require(`./webpack.config.${env}.js`)
+}
